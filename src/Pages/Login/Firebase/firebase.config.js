2634998e86bbd4cd9843e6fbd54c11e058a0f1ed
nmso2 <nmsubho@gmail.com>
2021-10-25 06:25:51 +0600

    update

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBDM1AXLQ_tj-QsD6Jg2mg-jN6lNDep8Zg",
-    authDomain: "react-genius-car-mechanics.firebaseapp.com",
-    projectId: "react-genius-car-mechanics",
-    storageBucket: "react-genius-car-mechanics.appspot.com",
-    messagingSenderId: "196594605898",
-    appId: "1:196594605898:web:d32885aaa70bd492566bb3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
